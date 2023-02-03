f08fe3f687f243a47fe8db942ef651719a3eaa95
jaikrishnavr <jaikrishnagokul@gmail.com>
2023-02-03 19:39:33 +0530

    github commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+      <div>Home
+
+          <img src='https://c.ndtvimg.com/2022-06/hsl9rb5_bts_650x300_02_June_22.jpg?im=Resize=(1230,900)'/>
+
+      </div>
+  )
+}
+
+export default Home
